d41273b8460dbe874f86acab94d216ed438c2b14
Ananya <ananyaagrawal073@gmail.com>
2024-07-05 17:26:32 +0530

    commit
    commit

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import {dbConnection } from "./database/dbConnection.js";
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./routes/userRouter.js";
+
 
 const app = express();
 dotenv.config({ path: "./config/config.env" });
@@ -15,6 +18,8 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/user", userRouter);
 dbConnection();
 
 export default app;
